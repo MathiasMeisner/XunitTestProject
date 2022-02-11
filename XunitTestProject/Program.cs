9971c6d88aecad97b7a73179c8ee94a382170b93
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2022-02-11 08:41:20 +0100

    Added Calculator.cs
    Simple methods.

--- XunitTestProject/Program.cs
+++ XunitTestProject/Program.cs
@@ -6,7 +6,7 @@ namespace XunitTestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worllld!");
         }
     }
 }
