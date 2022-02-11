aa0aa0232c3c82f0b466bba90c4760b5336eea37
MathiasMeisner <62882868+MathiasMeisner@users.noreply.github.com>
2022-02-11 08:59:04 +0100

    Added StringExample.cs + unit tests
    Class sets a valid length of text file name.
    Test checks for whether the name is valid or not.

---
+++ XunitTestProject.Tests/StringExampleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.IO;
+
+namespace XunitTestProject.Tests
+{
+    public class StringExampleTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = StringExample.ExampleLoadTextFile("This is a valid file name");
+
+            Assert.True(actual.Length > 10);
+        }
+
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldFail()
+        {
+            Assert.Throws<FileNotFoundException>(() => StringExample.ExampleLoadTextFile(""));
+        }
+    }
+}
